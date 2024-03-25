7a51c4d11ed154ebb1a139836436446f37598618
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-25 07:25:03 +0530

    Added editProfile backend API

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,12 +11,8 @@ const VerificationToken = require("../model/verificationToken");
 const { isValidObjectId } = require("mongoose");
 const jwt = require("jsonwebtoken");
 const { updatePercentilesOnQuizDeactivation } = require("../utils/quiz");
-const { formatDate } = require("../utils/date");
 const { progressBar } = require("../utils/progress");
 const {
-  calculateTopPercent,
-  calculateLabelsAndData,
-  calculatePercentilesOfEachBar,
   getUserIQScoreHistory,
   currentTopPercentOfUser,
   getSolvedQuizzesCount,
@@ -28,7 +24,7 @@ const registerUser = async (req, res) => {
   //console.log(req.body);
   const { name, email, phone, password, cpassword, inGameName } = req.body;
 
-  if (name || !email || !phone || !password || !cpassword || !inGameName)
+  if (!name || !email || !phone || !password || !cpassword || !inGameName)
     return res.status(422).json({ error: "Please fill the required field" });
   // inGameName cannot have spaces
   if (inGameName.includes(" "))
@@ -150,15 +146,13 @@ const logoutUser = async (req, res) => {
 };
 
 const loginCheck = async (req, res) => {
-  //console.log("auth");
-  //console.log(req.user);
   const user = await User.findById(req.user._id);
   res.status(201).send(user);
 };
 
 const verifyUser = async (req, res) => {
   const { otp, email } = req.body;
-  //console.log(req.body);
+
   const forgotPassword = req.query.forgotPassword;
   try {
     const user = await User.findOne({ email: email });
@@ -267,7 +261,7 @@ const forgotPassword = async (req, res) => {
 const handleGoogleLogin = async (req, res) => {
   const { credentialResponse, inGameName } = req.body;
   const credential = credentialResponse.credential;
-  //console.log(credential);
+
   try {
     const userInfo = jwt.decode(credential);
     let user = await User.findOne({
@@ -478,141 +472,6 @@ const calculateUserIQScores = async (req, res) => {
   }
 };
 
-// // Controller function to get user's IQ score history
-// const getUserIQScoreHistory = async (req, res) => {
-//   try {
-//     const userId = req.user._id; // Assuming you pass userId in the URL parameters
-
-//     // Find the user by ID
-//     const user = await User.findById(userId);
-
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-
-//     // Populate the dailyIQScores array to get the actual IQ score documents
-//     await user.populate("dailyIQScores");
-
-//     // Extract relevant information from the populated array
-//     const iqScoresHistory = user.dailyIQScores.map((score) => ({
-//       date: formatDate(score.date),
-//       IQScore: score.IQ_score,
-//       dailyRank: score.dailyRank,
-//     }));
-//     const sortedIQScoresHistory = iqScoresHistory.sort((a, b) => {
-//       // Convert the date strings to Date objects for comparison
-//       const dateA = new Date(a.date);
-//       const dateB = new Date(b.date);
-//       return dateA - dateB; // dates are equal
-//     });
-//     // Send the IQ score history to the frontend
-//     res.status(200).json({ IQ_score_history: sortedIQScoresHistory });
-//   } catch (error) {
-//     console.error("Error fetching user IQ score history:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
-// // Controller function to Current Percentile in IQ Scores
-// const currentTopPercentOfUser = async (req, res) => {
-//   try {
-//     const userID = req.user._id;
-//     const users = await User.find({});
-//     const user = await User.findById(userID);
-//     const USER_IQ = user.IQ_score;
-//     const IQScores = users.filter((u) => u.IQ_score > 0).map((u) => u.IQ_score);
-//     const Top_Percentage = calculateTopPercent(USER_IQ, IQScores);
-//     const { filteredLabels, filteredIQData } = calculateLabelsAndData(IQScores);
-//     const percentileData = calculatePercentilesOfEachBar(
-//       IQScores,
-//       filteredLabels,
-//       filteredIQData
-//     );
-
-//     res.status(200).json({
-//       Top_Percentage,
-//       percentileData,
-//       filteredLabels,
-//       filteredIQData,
-//       USER_IQ,
-//     });
-//   } catch (error) {
-//     console.error("Error fetching user IQ score history:", error);
-//   }
-// };
-
-// const solvedQuizzesCount = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-//     const user = await User.findById(userId);
-//     const totalSolvedQuiz = user.quizAttempts.length;
-//     const users = await User.find({});
-//     const easyQuizzesCount = user.easyQuizCount;
-//     const mediumQuizzesCount = user.mediumQuizCount;
-//     const hardQuizzesCount = user.hardQuizCount;
-//     // number of users solved less than easyQuizzesCount
-//     const easyBeatsPercentage =
-//       (users.filter((u) => u.easyQuizCount < easyQuizzesCount).length /
-//         users.length) *
-//       100;
-//     const medBeatsPercentage =
-//       (users.filter((u) => u.mediumQuizCount < mediumQuizzesCount).length /
-//         users.length) *
-//       100;
-//     const hardBeatsPercentage =
-//       (users.filter((u) => u.hardQuizCount < hardQuizzesCount).length /
-//         users.length) *
-//       100;
-//     res.status(200).json({
-//       solvedQuizzesCount: totalSolvedQuiz,
-//       easy: { easyQuizzesCount, easyBeatsPercentage },
-//       medium: { mediumQuizzesCount, medBeatsPercentage },
-//       hard: { hardQuizzesCount, hardBeatsPercentage },
-//     });
-//   } catch (error) {
-//     console.error("Error fetching user IQ score history:", error.message);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
-// const dailActivity = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-//     const user = await User.findById(userId);
-//     const quizAttempts = await QuizAttempt.find({ user: userId });
-
-//     const dailyActivity = quizAttempts.map((attempt) => ({
-//       date: attempt.createdAt,
-//     }));
-
-//     res.status(200).json({ dailyActivity });
-//   } catch (error) {
-//     console.error("Error fetching user Daily activity history:", error.message);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
-// const calculateUserRank = async (req, res) => {
-//   try {
-//     const userId = req.user._id;
-//     const users = await User.find({}).sort({ IQ_score: -1 });
-//     //console.log(users);
-//     const userIndex = users.findIndex(
-//       (user) => user._id.toString() === userId.toString()
-//     );
-//     //console.log(userIndex);
-//     if (userIndex === -1) {
-//       return res.status(404).json({ error: "User not found" });
-//     }
-//     const rank = userIndex + 1;
-
-//     return res.status(200).json({ rank });
-//   } catch (error) {
-//     res.status(500).json({ error: "Error calculate the Rank" });
-//     console.log(error.message);
-//   }
-// };
-
 const leaderBoard = async (req, res) => {
   try {
     const users = await User.find({})
@@ -705,6 +564,24 @@ const profile = async (req, res) => {
   }
 };
 
+const editProfile = async (req, res) => {
+  const { name, bio, pic } = req.body;
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    user.name = name;
+    user.bio = bio;
+    user.pic = pic;
+    await user.save();
+    res.status(200).json({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.error("Error editing user profile:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
@@ -715,11 +592,7 @@ module.exports = {
   forgotPassword,
   handleGoogleLogin,
   calculateUserIQScores,
-  //getUserIQScoreHistory,
-  //currentTopPercentOfUser,
-  // solvedQuizzesCount,
-  // dailActivity,
-  //calculateUserRank,
+  editProfile,
   leaderBoard,
   profile,
 };
