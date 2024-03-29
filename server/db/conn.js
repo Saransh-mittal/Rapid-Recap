a93cc89b8c32e155ad9d665ceb248bffb7f94751
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-03-29 10:07:36 +0530

    added new articles in db

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
 // const Article = require("../model/articleSchema");
-// const articlesData = require("../../articleEntertainment.json");
+// const articlesData = require("../../articleHealth.json");
 
 mongoose
   .connect(DB)
@@ -25,5 +25,5 @@ mongoose
 //   }
 // }
 
-// Call the function to save articles to the database
+// // Call the function to save articles to the database
 // saveArticlesToDB();
