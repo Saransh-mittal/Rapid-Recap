077eddf4031d876aaafb857c663b11648625ed59
Gaurav Sahu <gaurav30.sahu@gmail.com>
2024-04-03 09:57:40 +0530

    Shep in Learderboard

--- client/src/screens/LeaderBoard.jsx
+++ client/src/screens/LeaderBoard.jsx
@@ -13,21 +13,25 @@ import {
   Tr,
   useToast,
 } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
 import medalIcon from "../assets/medal.png";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import { useShepherdTour } from "react-shepherd";
 import Loading from "../components/miscellaneous/Loading";
 
 const LeaderBoard = () => {
   const toast = useToast();
   const [isLoading, setIsLoading] = useState(true);
   const [leaders, setLeaders] = useState(null);
+  const tour = useShepherdTour({
+    tourOptions,
+    steps: stepsLeaderBoard,
+  });
 
-  const fetchLearderBoard = async () => {
+  const fetchLeaderBoard = async () => {
     try {
       const response = await axios.get("/api/user/leaderboard");
-      //console.log(response.data.users);
-      setLeaders(() => response.data.users);
+      setLeaders(response.data.users);
     } catch (error) {
       toast({
         title: "Error",
@@ -42,42 +46,27 @@ const LeaderBoard = () => {
       setIsLoading(false);
     }
   };
+
   useEffect(() => {
-    fetchLearderBoard();
+    fetchLeaderBoard();
   }, []);
+
+  useEffect(() => {
+    tour.start();
+  }, []);
+
   return (
     <Flex minH={"85vh"} justifyContent={"center"}>
-      <Flex
-        margin={"20px"}
-        justifyContent={"center"}
-        w={"80%"}
-        flexDirection={"column"}
-      >
+      <Flex margin={"20px"} justifyContent={"center"} w={"80%"} flexDirection={"column"}>
         {isLoading ? (
           <Loading />
         ) : (
           <>
             <Heading>
-              <Flex
-                alignItems={"center"}
-                gap={"10px"}
-                justifyContent={"center"}
-              >
-                <Image
-                  src={medalIcon}
-                  alt="Rating"
-                  width={"35px"}
-                  height={"35px"}
-                  bg={"none"}
-                />
+              <Flex alignItems={"center"} gap={"10px"} justifyContent={"center"}>
+                <Image src={medalIcon} alt="Rating" width={"35px"} height={"35px"} bg={"none"} />
                 LEADERBOARD
-                <Image
-                  src={medalIcon}
-                  alt="Rating"
-                  width={"35px"}
-                  height={"35px"}
-                  bg={"none"}
-                />
+                <Image src={medalIcon} alt="Rating" width={"35px"} height={"35px"} bg={"none"} />
               </Flex>
             </Heading>
             <TableContainer width={"100%"}>
@@ -85,7 +74,6 @@ const LeaderBoard = () => {
                 <TableCaption color={"white"} placement="top">
                   "Where Champions Stand Out!"
                 </TableCaption>
-
                 <Thead>
                   <Tr boxShadow={"dark-lg"}>
                     <Th textAlign={"center"} bg={"green.300"} color={"white"}>
@@ -108,60 +96,56 @@ const LeaderBoard = () => {
                     </Th>
                   </Tr>
                 </Thead>
-
                 <Tbody marginTop={"20px"}>
-                  {leaders.length > 0 &&
-                    leaders.map((leader, index) => {
-                      return (
-                        <Tr height={"80px"} key={leader._id}>
-                          <Td textAlign={"center"}>
-                            <Flex
-                              justifyContent={"center"}
-                              alignItems={"center"}
-                              bgGradient="linear(to-b, #1a1527, #0e0c16 88%, #0e0c16 99%)"
-                              p={2}
-                              gap={"35px"}
-                              borderRadius="md"
+                  {leaders.map((leader, index) => (
+                    <Tr height={"80px"} key={leader._id}>
+                      <Td textAlign={"center"}>
+                        <Flex
+                          justifyContent={"center"}
+                          alignItems={"center"}
+                          bgGradient="linear(to-b, #1a1527, #0e0c16 88%, #0e0c16 99%)"
+                          p={2}
+                          gap={"35px"}
+                          borderRadius="md"
+                        >
+                          {index + 1}
+                          <Flex
+                            border={"5px solid gold"}
+                            style={{ transform: "rotate(45deg)" }}
+                            w="45px"
+                            h="45px"
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                            bg={"blue.200"}
+                            position={"relative"}
+                            overflow={"hidden"}
+                          >
+                            <Box
+                              position={"absolute"}
+                              h="50px"
+                              w="50px"
+                              style={{
+                                transform: "rotate(-45deg)",
+                              }}
                             >
-                              {index + 1}
-                              <Flex
-                                border={"5px solid gold"}
-                                style={{ transform: "rotate(45deg)" }}
-                                w="45px"
-                                h="45px"
-                                justifyContent={"center"}
-                                alignItems={"center"}
-                                bg={"blue.200"}
-                                position={"relative"}
-                                overflow={"hidden"}
-                              >
-                                <Box
-                                  position={"absolute"}
-                                  h="50px"
-                                  w="50px"
-                                  style={{
-                                    transform: "rotate(-45deg)",
-                                  }}
-                                >
-                                  <Image
-                                    h={"100%"}
-                                    w={"100%"}
-                                    src={leader.pic}
-                                    alt="Dan Abramov"
-                                    objectFit={"cover"}
-                                  />
-                                </Box>
-                              </Flex>
-                            </Flex>
-                          </Td>
-                          <Td textAlign="center">{leader.name}</Td>
-                          <Td textAlign="center">{leader.inGameName}</Td>
-                          <Td textAlign="center">{leader.IQ_score}</Td>
-                          <Td textAlign="center">{leader.quizSubmissions}</Td>
-                          <Td textAlign="center">{leader.RQM_avg}</Td>
-                        </Tr>
-                      );
-                    })}
+                              <Image
+                                h={"100%"}
+                                w={"100%"}
+                                src={leader.pic}
+                                alt="Dan Abramov"
+                                objectFit={"cover"}
+                              />
+                            </Box>
+                          </Flex>
+                        </Flex>
+                      </Td>
+                      <Td textAlign="center">{leader.name}</Td>
+                      <Td textAlign="center">{leader.inGameName}</Td>
+                      <Td textAlign="center">{leader.IQ_score}</Td>
+                      <Td textAlign="center">{leader.quizSubmissions}</Td>
+                      <Td textAlign="center">{leader.RQM_avg}</Td>
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
             </TableContainer>
@@ -173,3 +157,36 @@ const LeaderBoard = () => {
 };
 
 export default LeaderBoard;
+
+const stepsLeaderBoard = [
+  {
+    id: "introduction",
+    attachTo: { element: "h2", on: "bottom" },
+    title: "Welcome to Leaderboard",
+    text: "This is where the champions stand out! Click 'Next' to explore more.",
+    buttons: [{ text: "Next", type: "next" }],
+  },
+  {
+    id: "exploring-table",
+    attachTo: { element: "thead", on: "bottom" },
+    title: "Exploring the Leaderboard Table",
+    text: "Here you can see the details of the top performers. Take a look and understand the columns.",
+    buttons: [{ text: "Back", type: "back" }, { text: "Next", type: "next" }],
+  },
+  {
+    id: "understanding-entry",
+    attachTo: { element: "tbody tr:nth-child(1)", on: "top" },
+    title: "Understanding Leaderboard Entries",
+    text: "Each row represents a user with their respective stats. Click 'Exit' to end the tour.",
+    buttons: [{ text: "Exit", type: "cancel" }],
+  },
+];
+
+const tourOptions = {
+  defaultStepOptions: {
+    cancelIcon: {
+      enabled: true,
+    },
+  },
+  useModalOverlay: true,
+};
