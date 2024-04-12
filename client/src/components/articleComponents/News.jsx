1cdc773b1b0441dd16474f1295c9d5a3c2d32aee
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-04-12 10:48:20 +0530

    Fixed Article page mobile on Back bug

--- client/src/components/articleComponents/News.jsx
+++ client/src/components/articleComponents/News.jsx
@@ -1,4 +1,4 @@
-import { useRef, useContext, useEffect } from "react";
+import { useRef, useContext, useEffect, useState } from "react";
 import { AppContext } from "../../contextAPI/appContext";
 import { Button, Image } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import imageData from "../../assets/AltNewsImage";
 
 const News = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const { state, dispatch } = useContext(AppContext);
   const data = state.news;
   const alt_image = imageData.find(
@@ -15,8 +16,21 @@ const News = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (window.innerWidth < 768) navigate(`/article/${data._id}`);
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
   }, [window.innerWidth]);
+
+  useEffect(() => {
+    if (isMobile) {
+      dispatch({ type: "showModal", payloadModal: false });
+      navigate(`/article/${data._id}`);
+    }
+  }, [isMobile, data._id]);
+
+  if (isMobile) {
+    return null; // Prevent rendering the News component if redirecting
+  }
 
   return (
     <div
@@ -24,28 +38,32 @@ const News = () => {
       style={{
         backgroundColor: "#1a1a2e",
         color: "#253547",
-        minHeight: "90vh", 
+        minHeight: "90vh",
         // maxWidth: "80vh",
       }}
-      >
+    >
       <Image
         src={data.imgURL ? data.imgURL : alt_image}
-        style={{ width: "92%", marginTop: "1rem", maxWidth:"80vh" }}
+        style={{ width: "92%", marginTop: "1rem", maxWidth: "80vh" }}
         onError={(e) => {
           e.target.onerror = null;
           e.target.src = alt_image;
         }}
-        />
-      <div 
+      />
+      <div
         className="d-flex justify-content-center flex-column align-items-center"
-      style={{ width: "92%" }}>
+        style={{ width: "92%" }}
+      >
         <h1
           className="mt-3 mb-3"
-          style={{ color: "#f0f0f0", textAlign: "center", maxWidth:"150vh" }}
+          style={{ color: "#f0f0f0", textAlign: "center", maxWidth: "150vh" }}
         >
           {data.title}
         </h1>
-        <p className="mb-2" style={{ color: "#f0f0f0", textAlign: "center", maxWidth:"150vh" }}>
+        <p
+          className="mb-2"
+          style={{ color: "#f0f0f0", textAlign: "center", maxWidth: "150vh" }}
+        >
           {data.mainText.length > 1000
             ? `${data.mainText.substring(0, 1000)}...`
             : data.mainText}
