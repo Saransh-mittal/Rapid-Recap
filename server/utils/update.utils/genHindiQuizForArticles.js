726e4ae301fe1774b0bf13d3c84b16891b4a6f05
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-04-20 08:23:50 +0530

    :sparkles:

--- server/utils/update.utils/genHindiQuizForArticles.js
+++ server/utils/update.utils/genHindiQuizForArticles.js
@@ -7,7 +7,7 @@ const { progressBar } = require("../progress");
 
 const genHindiQuizForArticles = async () => {
   const twoDaysAgo = new Date();
-  twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+  twoDaysAgo.setDate(twoDaysAgo.getDate() - 5);
 
   // Construct the aggregation pipeline
   const pipeline = [
@@ -24,14 +24,30 @@ const genHindiQuizForArticles = async () => {
     const progress = progressBar(articles.length);
     console.log("\nGenerating Hindi quizzes for articles...\n");
     for (let article of articles) {
-      const articleId = article._id;
-      let fullQuiz;
-      if (article.quiz && article.quiz.length > 0) {
-        fullQuiz = await findQuizByLanguage({
-          language: "hi",
-          articleId,
-        });
-        if (!fullQuiz) {
+      try {
+        const articleId = article._id;
+        const { hindiTitle, hindiAuthor, hindiMainText } = article;
+        if (!hindiTitle || !hindiAuthor || !hindiMainText) {
+          continue;
+        }
+        let fullQuiz;
+        if (article.quiz && article.quiz.length > 0) {
+          fullQuiz = await findQuizByLanguage({
+            language: "hi",
+            articleId,
+          });
+          if (!fullQuiz) {
+            fullQuiz = await generateQuestionsForHindiQuiz({
+              title: hindiTitle,
+              author: hindiAuthor,
+              mainText: hindiMainText,
+              articleId,
+            });
+          }
+
+          // Now you have a valid fullQuiz
+          // Proceed with your code...
+        } else {
           fullQuiz = await generateQuestionsForHindiQuiz({
             title: hindiTitle,
             author: hindiAuthor,
@@ -39,17 +55,10 @@ const genHindiQuizForArticles = async () => {
             articleId,
           });
         }
-
-        // Now you have a valid fullQuiz
-        // Proceed with your code...
-      } else {
-        fullQuiz = await generateQuestionsForHindiQuiz({
-          title: hindiTitle,
-          author: hindiAuthor,
-          mainText: hindiMainText,
-          articleId,
-        });
+      } catch (error) {
+        console.log(`Error processing article ${article._id}: ${error}`);
       }
+
       progress();
     }
     console.log("\nHindi quizzes generated successfully!\n");
