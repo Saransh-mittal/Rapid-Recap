dc8d99c6ce43866e58e4e4d1a207316c7fad16d7
Gaurav Sahu <gaurav30.sahu@gmail.com>
2024-05-12 15:06:46 +0530

    Testing

--- client/src/components/Header-Footer/Navbar.jsx
+++ client/src/components/Header-Footer/Navbar.jsx
@@ -19,7 +19,7 @@ import { HamburgerIcon, CloseIcon, EmailIcon } from "@chakra-ui/icons";
 import Categories from "./Categories";
 import NotificationDrawer from "./Inbox/NotificationDrawer";
 import DailyStreakModal from "../streakComponents/DailyStreakModal";
-import { motion } from "framer-motion"; // Not being used in this snippet
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const navItems = [
