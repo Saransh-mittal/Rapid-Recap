a94db4b368731aecbbb3093ad80d76ae060807b5
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-05-23 10:47:02 +0530

    Beta Version 1.1.16 testing -> saransh

--- server/scheduler/extractNews.js
+++ server/scheduler/extractNews.js
@@ -1,7 +1,20 @@
 const cron = require("node-cron");
 const { extractNewsUtilityFunc } = require("../utils/article.utils");
+const moment = require("moment-timezone");
 
-cron.schedule("0 2 * * *", async () => {
+const currentDate = moment().format("YYYY-MM-DD");
+const timeIST = moment.tz(`${currentDate} 11:10`, "Asia/Kolkata"); // Use the current date
+const timeUTC = timeIST.clone().tz("UTC");
+
+// Step 2: Convert UTC to local time of the machine
+const timeLocal = timeUTC.clone().local();
+const localHour = timeLocal.hour();
+const localMinute = timeLocal.minute();
+
+// Step 3: Create a cron pattern based on local time
+const cronPattern = `${localMinute} ${localHour} * * *`;
+
+cron.schedule(cronPattern, async () => {
   try {
     // Call your function here
     const { result, articlesSavedPerCategory } = await extractNewsUtilityFunc();
--- server/scheduler/userIQScoreScheduler.js
+++ server/scheduler/userIQScoreScheduler.js
@@ -1,7 +1,19 @@
 const cron = require("node-cron");
 const dailyUserIQCalc = require("../utils/dailyUserIQCalc.utils");
+const moment = require("moment-timezone");
 
-cron.schedule("0 0 * * *", async () => {
+const currentDate = moment().format("YYYY-MM-DD");
+const timeIST = moment.tz(`${currentDate} 12:10`, "Asia/Kolkata"); // Use the current date
+const timeUTC = timeIST.clone().tz("UTC");
+
+// Step 2: Convert UTC to local time of the machine
+const timeLocal = timeUTC.clone().local();
+const localHour = timeLocal.hour();
+const localMinute = timeLocal.minute();
+
+// Step 3: Create a cron pattern based on local time
+const cronPattern = `${localMinute} ${localHour} * * *`;
+cron.schedule(cronPattern, async () => {
   try {
     // Call your function here
     await dailyUserIQCalc();
