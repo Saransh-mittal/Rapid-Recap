95c0a92a98a3794e9d148fdf95bf01500121be5a
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-05-23 15:05:41 +0530

    Beta Version 1.1.16

--- scheduler/userIQScoreScheduler.js
+++ scheduler/userIQScoreScheduler.js
@@ -4,7 +4,7 @@ const moment = require("moment-timezone");
 const updateBots = require("../utils/bot.utils/updateBots");
 
 const currentDate = moment().format("YYYY-MM-DD");
-const timeIST = moment.tz(`${currentDate} 00:00`, "Asia/Kolkata"); // Use the current date
+const timeIST = moment.tz(`${currentDate} 15:10`, "Asia/Kolkata"); // Use the current date
 const timeUTC = timeIST.clone().tz("UTC");
 
 // Step 2: Convert UTC to local time of the machine
