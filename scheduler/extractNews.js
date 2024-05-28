dd820b8fc531ff59938e978a46e53e41e0a6bc41
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-05-28 10:53:20 +0530

    Beta version 1.1.17 news update notification bug fixed for only production branch

--- scheduler/extractNews.js
+++ scheduler/extractNews.js
@@ -18,7 +18,8 @@ const cronPattern = `${localMinute} ${localHour} * * *`;
 cron.schedule(cronPattern, async () => {
   try {
     // Call your function here
-    const { result, articlesSavedPerCategory } = await extractNewsUtilityFunc();
+    const { result, articlesSavedPerCategory, notificationCategories } =
+      await extractNewsUtilityFunc();
     if (result.length > 0) {
       const title = `📢 New ${notificationCategories} Content Alert! 📰`;
       const body =
