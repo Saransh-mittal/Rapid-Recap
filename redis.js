a003c5c0ca24ead3250548c05212ce0c25eeea59
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-07-24 16:31:09 +0530

    Beta Version 1.2.5 using verbose redis connection

--- redis.js
+++ redis.js
@@ -1,4 +1,18 @@
 const Redis = require("ioredis");
-const redis = new Redis(process.env.REDIS_URL);
+
+const redis = new Redis({
+  host: process.env.REDISHOST,
+  port: process.env.REDISPORT,
+  username: process.env.REDISUSER,
+  password: process.env.REDISPASSWORD,
+});
+
+redis.on("error", (error) => {
+  console.error("Redis connection error:", error);
+});
+
+redis.on("connect", () => {
+  console.log("Successfully connected to Redis");
+});
 
 module.exports = { redis };
