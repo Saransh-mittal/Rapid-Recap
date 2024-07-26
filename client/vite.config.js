48202342eec233023674711e054df2ec8d4f749d
Saransh Mittal <20ucs174@lnmiit.ac.in>
2024-07-26 12:51:14 +0530

    Added manifest -> mobileApp--feature

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: "esnext",
   },
   server: {
-    host: true, // This allows external connections
+    host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Replace with your backend server URL
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Rapid Recap",
+        short_name: "RapidRecap",
+        description: "Stay Informed, Stay Ahead with Rapid Recap",
+        theme_color: "#1a1527",
+        background_color: "#0f0d15",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/images/rr.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/rr.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
 });
