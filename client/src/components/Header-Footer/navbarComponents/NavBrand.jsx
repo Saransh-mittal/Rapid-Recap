b5b4fe5c5452f12679e6fc9ae192d427a215ff70
Saransh-mittal <20ucs174@lnmiit.ac.in>
2024-07-31 10:54:04 +0530

    User logged in successfully navigated to home -> saransh

--- client/src/components/Header-Footer/navbarComponents/NavBrand.jsx
+++ client/src/components/Header-Footer/navbarComponents/NavBrand.jsx
@@ -1,12 +1,15 @@
 import { Flex, Image } from '@chakra-ui/react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import RR from '/images/rrlogo.webp'
 import Heading from '../../miscellaneous/HeadingComponent'
+import { AppContext } from '../../../contextAPI/appContext'
 
 const NavBrand = ({ isHamburgerOpen }) => {
+  const { state } = useContext(AppContext)
+  const notLoggedIn = state.show
   return (
-    <NavLink to="/" className={`navbar-brand`}>
+    <NavLink to={notLoggedIn ? '/' : '/get-started'} className={`navbar-brand`}>
       <Flex position={!isHamburgerOpen ? 'absolute' : 'relative'}>
         <Image
           src={RR}
