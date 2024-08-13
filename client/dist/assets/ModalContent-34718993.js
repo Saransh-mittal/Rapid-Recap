69570b180b57192eecd23ef211a98563451db22d
Saransh-mittal <20ucs174@lnmiit.ac.in>
2024-08-13 12:43:43 +0530

    Beta Version 1.2.31 Bug fixed for register page to open

---
+++ client/dist/assets/ModalContent-34718993.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-d20c110d.js";const s=({children:t})=>o.jsx("div",{children:t});export{s as default};
---
+++ client/dist/assets/chunk-RAWN7VJ3-0f79f5ec.js
@@ -0,0 +1,1 @@
+import{b as l}from"./chunk-JQMJHPZH-93b8e777.js";import{y as c,l as m,j as f,m as n}from"./index-d20c110d.js";var d=c((s,o)=>{const{className:e,...a}=s,t=m("chakra-modal__footer",e),r={display:"flex",alignItems:"center",justifyContent:"flex-end",...l().footer};return f.jsx(n.footer,{ref:o,...a,__css:r,className:t})});d.displayName="ModalFooter";export{d as M};
