83a8ffda77156bfdab7fa4ab8e64b8f488f00d93
Saransh-mittal <20ucs174@lnmiit.ac.in>
2024-08-15 18:01:25 +0530

    Beta Version 1.2.34 bug resolved

--- controllers/article.js
+++ controllers/article.js
@@ -19,14 +19,6 @@ const { formatDate } = require('../utils/miscellaneous.utils')
 const Quiz = require('../model/quizSchema')
 const NewsAPI = require('newsapi')
 const asyncHandler = require('express-async-handler')
-const { TfIdf } = require('natural')
-const cosineDistances = require('compute-cosine-distance')
-const {
-  startSession,
-  commitSession,
-  abortSession,
-  endSession,
-} = require('../db/session')
 const { default: mongoose } = require('mongoose')
 
 const allArticles = async (req, res) => {
