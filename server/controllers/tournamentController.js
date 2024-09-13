930bc13cc2a73c40db64fbe5fa44ed92fdc59861
Saransh <20ucs174@lnmiit.ac.in>
2024-09-13 17:13:51 +0530

    Ready to test -> TournamentMode--feature

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -72,7 +72,6 @@ const getLatestTournament = asyncHandler(async (req, res) => {
     registeredCount: await TournamentRegistration.countDocuments({
       tournament: tournament._id,
     }),
-    status: 'completed',
     isRegistered: !!userRegistration,
     // status: 'registration',
     selectedCategories: userRegistration
