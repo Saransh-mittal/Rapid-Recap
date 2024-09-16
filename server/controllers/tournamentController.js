71f35b4974739fc45d28533b75798c38d978d077
Gaurav701a <gaurav30.sahu@gmail.com>
2024-09-16 16:07:27 +0530

    minor adjustments done->TournamentMode--feature

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -73,7 +73,7 @@ const getLatestTournament = asyncHandler(async (req, res) => {
       tournament: tournament._id,
     }),
     isRegistered: !!userRegistration,
-    status: 'registration',
+    // status: 'registration',
     selectedCategories: userRegistration
       ? userRegistration.selectedCategories
       : [],
