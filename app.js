f59bcd72e00a6077e0a1bcead304f0c885c1a339
Saransh <20ucs174@lnmiit.ac.in>
2024-11-14 16:07:56 +0530

    Beta Version 1.7.20 patch update and some bug fixes

--- app.js
+++ app.js
@@ -242,7 +242,7 @@ webpush.setVapidDetails(
 initBotTracking()
 
 // Load scheduler
-// require('./scheduler/setupCronJobs')
+require('./scheduler/setupCronJobs')
 
 // Setup routes and SSR
 async function initializeServer() {
