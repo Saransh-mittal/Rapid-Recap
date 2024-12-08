3bc557676f66e91e241ebeb3bceb35d385c25f7f
Gaurav Sahu <gaurav30.sahu@gmail.com>
2024-12-08 14:07:20 +0530

    Not user lang switcher added

--- client/src/components/articleComponents/ArticleHeader.jsx
+++ client/src/components/articleComponents/ArticleHeader.jsx
@@ -38,6 +38,7 @@ import { formatDate } from '../../utils/helper.utils'
 import AITagLine from './articleHeaderComponents/AITagLine'
 import { useFeatureDetection } from '../../utils/featureDetection'
 import useSafeSound from '../../customHooks/useSafeSound'
+import NotUserLangSwitcher from './NotUserLangSwitcher'
 
 const ArticleForm = React.lazy(() =>
   import('../dashboardComponents/ArticleManageComponents/ArticleForm'),
@@ -253,7 +254,7 @@ const ArticleHeader = ({
               w={'100%'}
             >
               {/* <AuthorInfo author={author} selectedLanguage={selectedLanguage} /> */}
-              <AITagLine />
+              <AITagLine t={t} />
               {isAdmin && (
                 <>
                   <EditIcon
@@ -319,7 +320,10 @@ const ArticleHeader = ({
               <Flex
                 justifyContent={{ base: 'flex-end', lg: 'flex-start' }}
                 w={{ base: '100%', lg: 'auto' }}
+                alignItems={'center'}
+                gap={2}
               >
+                <Flex>{!user && <NotUserLangSwitcher />}</Flex>
                 <Text fontSize={['sm', 'md', 'lg']} mb={0}>
                   {avgTimeRead} {t('timeToRead')} • <time>{formattedDate}</time>
                 </Text>
@@ -380,16 +384,11 @@ const ArticleHeader = ({
                 </MenuList>
               </Menu>
             </Box>
-            <Badge
-              colorScheme="purple"
-              variant="solid"
-              px={2}
-              py={1}
-              borderRadius="md"
-              fontSize="xs"
-            >
-              {t('featureInTesting')}
-            </Badge>
+            {!user && !isLargerThan768 && (
+              <Flex align={'flex-end'} w={'100%'}>
+                <NotUserLangSwitcher />
+              </Flex>
+            )}
           </Flex>
         </Flex>
         <ShareChatModal
