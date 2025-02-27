80beb69dc9a829bd347b76d0dff348eba6b7b5b4
Saransh <20ucs174@lnmiit.ac.in>
2025-02-27 19:05:57 +0530

    Alpha Version 1.4.6 removed cache headers from ssr middleware

--- app.js
+++ app.js
@@ -304,11 +304,11 @@ webpush.setVapidDetails(
 
 // Initialize bot tracking
 initBotTracking()
-// require('./scripts/script_prepare_article_data')()
-// const generateSitemap = require('./generate-sitemap')
-// generateSitemap()
-// const generateGoogleNewsSitemap = require('./google-sitemap-generator')
-// generateGoogleNewsSitemap()
+require('./scripts/script_prepare_article_data')()
+const generateSitemap = require('./generate-sitemap')
+generateSitemap()
+const generateGoogleNewsSitemap = require('./google-sitemap-generator')
+generateGoogleNewsSitemap()
 // Load scheduler
 require('./scheduler/setupCronJobs')
 // require('./scripts/analyzeArticleRelations')
