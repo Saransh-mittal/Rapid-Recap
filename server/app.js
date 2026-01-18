a22d24dddfaab8946332c8f9253ca458018eceb2
Saransh <20ucs174@lnmiit.ac.in>
2026-01-18 12:09:08 +0530

    switched on cron jobs -> RRConceptRemodelling

--- server/app.js
+++ server/app.js
@@ -207,7 +207,7 @@ app.use(errorHandler)
 // const { runCompleteWorkflow } = require('./scripts/runCompleteForgeWorkflow')
 // runCompleteWorkflow({waitForCompletion:true,skipScraping:false,skipProcessing:false})
 // require('./utils/deletion.utils/removeForgeData.del')
-// require('./scheduler/setupCronJobs')
+require('./scheduler/setupCronJobs')
 
 const PORT = process.env.PORT
 authRouter.use(cookieParser())
