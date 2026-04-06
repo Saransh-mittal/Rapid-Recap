29c6ac8eb56a3d2eb5f2b386977de1f526817de2
Saransh <20ucs174@lnmiit.ac.in>
2026-04-06 15:37:23 +0530

    feat: re-enable scheduler cron jobs in app.js

--- app.js
+++ app.js
@@ -390,7 +390,7 @@ const {
 // generateGoogleNewsSitemap()
 
 // Load scheduler
-// require('./scheduler/setupCronJobs')
+require('./scheduler/setupCronJobs')
 
 // ============== KEEP-ALIVE FUNCTIONALITY ==============
 // Check if we're running on Railway
