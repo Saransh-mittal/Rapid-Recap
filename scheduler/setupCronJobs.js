9eccd16f1333529353374e39f6b3250c6348b65b
Saransh <20ucs174@lnmiit.ac.in>
2026-04-10 08:13:13 +0530

    Disabled forge forgeSchedules from cron jobs

--- scheduler/setupCronJobs.js
+++ scheduler/setupCronJobs.js
@@ -4,10 +4,10 @@ const quickClashSchedules = require('./quickClashScheduleConfig')
 
 // Setup Forge workflow schedules
 const forgeSchedules = require('./forgeScheduleConfig')
-forgeSchedules.forEach(schedule => {
-  cron.schedule(schedule.cronPattern, schedule.task)
-  console.log(`Scheduled ${schedule.name} task`)
-})
+// forgeSchedules.forEach(schedule => {
+//   cron.schedule(schedule.cronPattern, schedule.task)
+//   console.log(`Scheduled ${schedule.name} task`)
+// })
 // Setup Quick Clash schedules
 quickClashSchedules.forEach(schedule => {
   // Check if this schedule needs seconds-level precision
